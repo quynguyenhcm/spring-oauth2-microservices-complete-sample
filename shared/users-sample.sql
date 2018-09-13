d2e1a24b5f300c551e88fadd4c89bff1a83c84e1
nguyenledinhquy@gmail.com <dec1001@Q>
2018-09-13 18:22:20 +1200

    #Update dummy data script

--- shared/users-sample.sql
+++ shared/users-sample.sql
@@ -10,6 +10,7 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+USE users;
 
 -- Dumping structure for table users.authorities
 DROP TABLE IF EXISTS `authorities`;
